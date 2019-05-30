b8f24a8495a1719d2f8a4dab768e0a48e18c646d
Rupert Moran <rupert.moran.adcock@gmail.com>
2019-05-30 21:49:09 +1200

    Added progress bar and changed text
    progress bar now on project screen and text has been changed to relevant topics

--- project-js.js
+++ project-js.js
@@ -4,6 +4,9 @@ window.onload = function() {
 	var c = sessionStorage.getItem(cName);
 	if (c == 1) {
 		document.getElementById('td').innerHTML = 'COMPLETE';
+		var progBar = document.getElementById('prog-bar');
+		progBar.innerHTML = '100%';
+		progBar.style.width = '100%';
 	}	
 }
 	
